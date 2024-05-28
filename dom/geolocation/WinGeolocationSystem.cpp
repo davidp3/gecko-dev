652e8dfb6e5e32e0557bee63f8cca2c00ad10332
David P <daparks@mozilla.com>
2024-05-28 14:33:08 -0700

    Bug 1858871: Part 3 - Implement system geolocation permission UX on Windows r=win-reviewers!
    This implements PresentSystemSettings, LocationIsPermittedHint and
    SystemWillPromptForPermissionHint for Windows.  The Windows APIs are not always
    available -- most are currently only available in Windows 11 Canary builds
    (slated for September release).  In the event that APIs are not available,
    this should do nothing.  At present, this is detailed here:
    
    https://learn.microsoft.com/en-us/windows/win32/nativewifi/wi-fi-access-location-changes
    
    There are two issues that this is intended to handle:
    
    1) The system will display a one-time (or so) dialog to the user when
    Firefox requests geolocation but doesn't have permission.  For that
    case, we inform the user that they will be asked to grant location permission
    again.  This system dialog is only presented in versions of Windows that
    support all of the relevant APIs.
    
    2) We open system settings to the right page and post a cancelable modal
    dialog on the tab if the user grants geolocation to the page but
    geolocation permission isn't currently granted in the OS.  This
    case will not happen if case #1 did.  Unfortunately, we can't get
    information about the permission status without a location request on old
    versions of Windows, so this also does nothing unless the recent APIs are
    supported (in this case, AppCapability::CheckAccess).
    
    This work is necessitated not only by the new (occasional) system dialog but
    also by Microsoft's plans to block wifi scanning if geolocation isn't
    available.  We have used wifi scanning as part of a fallback when system
    geolocation isn't available -- that approach is no longer viable here.  A user
    would confusingly get repeated errors or very poor results (e.g. IP lookup
    results) without information as to why, if that happened.  The fallback
    remains useful on other platforms.

---
+++ dom/geolocation/WinGeolocationSystem.cpp
@@ -0,0 +1,282 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim: set ts=8 sts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "GeolocationSystem.h"
+#include "mozilla/dom/Promise.h"
+#include "mozilla/MozPromise.h"
+
+#include <windows.system.h>
+#include <windows.security.authorization.appcapabilityaccess.h>
+#include <wrl.h>
+
+namespace mozilla::dom::geolocation {
+
+using ABI::Windows::Foundation::GetActivationFactory;
+using ABI::Windows::Security::Authorization::AppCapabilityAccess::AppCapability;
+using ABI::Windows::Security::Authorization::AppCapabilityAccess::
+    AppCapabilityAccessChangedEventArgs;
+using ABI::Windows::Security::Authorization::AppCapabilityAccess::
+    IAppCapability;
+using ABI::Windows::Security::Authorization::AppCapabilityAccess::
+    IAppCapabilityAccessChangedEventArgs;
+using ABI::Windows::Security::Authorization::AppCapabilityAccess::
+    IAppCapabilityStatics;
+using Microsoft::WRL::Callback;
+using Microsoft::WRL::ComPtr;
+using Microsoft::WRL::Wrappers::HStringReference;
+
+namespace {
+
+template <typename TypeToCreate>
+ComPtr<TypeToCreate> CreateFromActivationFactory(const wchar_t* aNamespace) {
+  ComPtr<TypeToCreate> newObject;
+  GetActivationFactory(HStringReference(aNamespace).Get(), &newObject);
+  return newObject;
+}
+
+RefPtr<
+    ABI::Windows::Security::Authorization::AppCapabilityAccess::IAppCapability>
+GetWifiControlAppCapability() {
+  ComPtr<IAppCapabilityStatics> appCapabilityStatics = CreateFromActivationFactory<
+      IAppCapabilityStatics>(
+      RuntimeClass_Windows_Security_Authorization_AppCapabilityAccess_AppCapability);
+  NS_ENSURE_TRUE(appCapabilityStatics, nullptr);
+
+  using ABI::Windows::System::IUserStatics2;
+  ComPtr<IUserStatics2> userStatics =
+      CreateFromActivationFactory<IUserStatics2>(
+          RuntimeClass_Windows_System_User);
+  NS_ENSURE_TRUE(userStatics, nullptr);
+
+  using ABI::Windows::System::IUser;
+  RefPtr<IUser> user;
+  HRESULT hr = userStatics->GetDefault(getter_AddRefs(user));
+  if (FAILED(hr)) {
+    return nullptr;
+  }
+  NS_ENSURE_TRUE(user, nullptr);
+
+  RefPtr<IAppCapability> appCapability;
+  hr = appCapabilityStatics->CreateWithProcessIdForUser(
+      user, HStringReference(L"wifiControl").Get(), ::GetCurrentProcessId(),
+      getter_AddRefs(appCapability));
+  if (FAILED(hr)) {
+    return nullptr;
+  }
+  NS_ENSURE_TRUE(appCapability, nullptr);
+  return appCapability;
+}
+
+class LocationSettingsListener final : public nsISupports {
+ public:
+  NS_DECL_ISUPPORTS
+
+  void SetToken(EventRegistrationToken aToken) { mToken = aToken; }
+
+  void Stop() {
+    mIsStopped = true;
+    if (mIsStopped) {
+      return;
+    }
+
+    // If the promise to open system settings is still waiting, reject it.
+    if (mOpenPromise) {
+      mOpenPromise->Reject(NS_ERROR_FAILURE, __func__);
+      mOpenPromise = nullptr;
+    }
+
+    // If the promise resolved then we may be watching system settings.  Stop
+    // doing that, too.
+    if (mToken.value == 0) {
+      // Not currently watching system settings.
+      return;
+    }
+
+    RefPtr<IAppCapability> appCapability = GetWifiControlAppCapability();
+    if (NS_WARN_IF(!appCapability)) {
+      return;
+    }
+
+    appCapability->remove_AccessChanged(mToken);
+    mToken = {};
+  }
+
+  bool IsStopped() { return mIsStopped; }
+
+ protected:
+  virtual ~LocationSettingsListener() { Stop(); }
+
+  RefPtr<OpenSettingsPromise::Private> mOpenPromise;
+  EventRegistrationToken mToken = {};
+  bool mIsStopped = false;
+};
+
+NS_IMPL_ISUPPORTS0(LocationSettingsListener)
+
+already_AddRefed<OpenSettingsPromise::Private> OpenWindowsLocationSettings() {
+  using ABI::Windows::System::ILauncherStatics;
+  ComPtr<ILauncherStatics> launcherStatics =
+      CreateFromActivationFactory<ILauncherStatics>(
+          RuntimeClass_Windows_System_Launcher);
+  NS_ENSURE_TRUE(launcherStatics, nullptr);
+
+  using ABI::Windows::Foundation::IUriRuntimeClass;
+  using ABI::Windows::Foundation::IUriRuntimeClassFactory;
+  ComPtr<IUriRuntimeClassFactory> uriFactory =
+      CreateFromActivationFactory<IUriRuntimeClassFactory>(
+          RuntimeClass_Windows_Foundation_Uri);
+  NS_ENSURE_TRUE(uriFactory, nullptr);
+  RefPtr<IUriRuntimeClass> uri;
+  HRESULT hr = uriFactory->CreateUri(
+      HStringReference(L"ms-settings:privacy-location").Get(),
+      getter_AddRefs(uri));
+  if (FAILED(hr)) {
+    return nullptr;
+  }
+
+  RefPtr<ABI::Windows::Foundation::IAsyncOperation<bool>> handler;
+  hr = launcherStatics->LaunchUriAsync(uri, getter_AddRefs(handler));
+  if (FAILED(hr)) {
+    return nullptr;
+  }
+
+  RefPtr<OpenSettingsPromise::Private> promise =
+      new OpenSettingsPromise::Private(__func__);
+  using ABI::Windows::Foundation::IAsyncOperation;
+  using ABI::Windows::Foundation::IAsyncOperationCompletedHandler;
+  handler->put_Completed(
+      Callback<IAsyncOperationCompletedHandler<bool>>(
+          [promise](IAsyncOperation<bool>* asyncInfo, AsyncStatus status) {
+            unsigned char verdict = 0;
+            asyncInfo->GetResults(&verdict);
+            if (verdict) {
+              promise->Resolve(true, __func__);
+            } else {
+              promise->Reject(NS_ERROR_FAILURE, __func__);
+            }
+            return S_OK;
+          })
+          .Get());
+  return promise.forget();
+}
+
+Maybe<ABI::Windows::Security::Authorization::AppCapabilityAccess::
+          AppCapabilityAccessStatus>
+GetWifiControlAccess() {
+  using ABI::Windows::Security::Authorization::AppCapabilityAccess::
+      IAppCapabilityStatics;
+  auto appCapability = GetWifiControlAppCapability();
+  NS_ENSURE_TRUE(appCapability, Nothing());
+
+  using ABI::Windows::Security::Authorization::AppCapabilityAccess::
+      AppCapabilityAccessStatus;
+  AppCapabilityAccessStatus status;
+  HRESULT hr = appCapability->CheckAccess(&status);
+  if (FAILED(hr)) {
+    return Nothing();
+  }
+  return Some(status);
+}
+
+}  // namespace
+
+//-----------------------------------------------------------------------------
+
+bool SystemWillPromptForPermissionHint() {
+  auto wifiAccess = GetWifiControlAccess();
+  return wifiAccess ==
+         mozilla::Some(ABI::Windows::Security::Authorization::
+                           AppCapabilityAccess::AppCapabilityAccessStatus::
+                               AppCapabilityAccessStatus_UserPromptRequired);
+}
+
+bool LocationIsPermittedHint() {
+  auto wifiAccess = GetWifiControlAccess();
+  // This API wasn't available on earlier versions of Windows,
+  // so a failure to get the result means that location is permitted.
+  return wifiAccess.isNothing() ||
+         *wifiAccess ==
+             ABI::Windows::Security::Authorization::AppCapabilityAccess::
+                 AppCapabilityAccessStatus::AppCapabilityAccessStatus_Allowed;
+}
+
+already_AddRefed<OpenSettingsPromise::Private> PresentSystemSettings() {
+  RefPtr<OpenSettingsPromise::Private> openPromise =
+      OpenWindowsLocationSettings();
+  if (NS_WARN_IF(!openPromise)) {
+    return nullptr;
+  }
+
+  // We need two promises because openPromise is resolved when the settings
+  // window has opened and retPromise resolves when system permission is
+  // granted (and rejects when the user presses cancel in the modal dialog).
+  RefPtr<OpenSettingsPromise::Private> retPromise =
+      new OpenSettingsPromise::Private(__func__);
+
+  RefPtr<LocationSettingsListener> locationListener =
+      new LocationSettingsListener();
+  openPromise->Then(
+      GetCurrentSerialEventTarget(), __func__,
+      [locationListener, retPromise](bool aWasOpened) {
+        if (locationListener->IsStopped()) {
+          // We are already canceled.
+          return false;
+        }
+        if (!aWasOpened) {
+          return false;
+        }
+        RefPtr<IAppCapability> appCapability = GetWifiControlAppCapability();
+        if (!appCapability) {
+          return false;
+        }
+
+        EventRegistrationToken token{};
+
+        using ABI::Windows::Foundation::ITypedEventHandler;
+        using AccessChangedHandler =
+            ITypedEventHandler<AppCapability*,
+                               AppCapabilityAccessChangedEventArgs*>;
+
+        appCapability->add_AccessChanged(
+            Callback<AccessChangedHandler>(
+                [locationListener, retPromise](
+                    IAppCapability*, IAppCapabilityAccessChangedEventArgs*) {
+                  if (locationListener->IsStopped()) {
+                    // We are already canceled.  This watchpoint is ending.
+                    return S_OK;
+                  }
+                  if (LocationIsPermittedHint()) {
+                    retPromise->Resolve(kSystemPermissionGranted, __func__);
+                    locationListener->Stop();
+                  }
+                  return S_OK;
+                })
+                .Get(),
+            &token);
+
+        locationListener->SetToken(token);
+        return token.value != 0;
+      },
+      [locationListener, retPromise](nsresult) {
+        locationListener->Stop();
+        retPromise->Resolve(kSystemPermissionGranted, __func__);
+      });
+
+  retPromise->Then(
+      GetCurrentSerialEventTarget(), __func__,
+      [locationListener]() {
+        // We got system permission.  Nothing to do here.
+        MOZ_ASSERT(locationListener->IsStopped());
+      },
+      [locationListener]() {
+        // We were canceled or got an error.  Make sure we stop watching the
+        // system setting.
+        locationListener->Stop();
+      });
+  return retPromise.forget();
+}
+
+}  // namespace mozilla::dom::geolocation
